cc4fd59c921cf4832c3fba42c3b266678ad288e6
artemut <zavarza555@yandex.ru>
2022-10-03 23:33:57 +0300

    https

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ import runtimeEnv from '@mars/heroku-js-runtime-env';
 
 const env = runtimeEnv();
 const config = {
-  apiBasePath: env.REACT_APP_API_BASE_PATH || 'http://62.84.120.87',
+  apiBasePath: env.REACT_APP_API_BASE_PATH || 'https://62.84.120.87',
   reactAppMode: process.env.REACT_APP_MODE || 'dev',
 };
 
